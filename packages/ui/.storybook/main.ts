d8bed447708d941133604e9fab03d46ec564cc53
David Stockley <magicspon@gmail.com>
2023-11-02 18:46:34 +0000

    chore: scaffold

---
+++ packages/ui/.storybook/main.ts
@@ -0,0 +1,34 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+
+import { join, dirname, resolve } from 'path'
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+	return dirname(require.resolve(join(value, 'package.json')))
+}
+const config: StorybookConfig = {
+	stories: [
+		'../stories/**/*.mdx',
+		'../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+	],
+	addons: [
+        getAbsolutePath('@storybook/addon-links'),
+        getAbsolutePath('@storybook/addon-essentials'),
+        getAbsolutePath('@storybook/addon-onboarding'),
+        getAbsolutePath('@storybook/addon-interactions'),
+        '@storybook/addon-styling-webpack'
+    ],
+	framework: {
+		name: '@storybook/nextjs',
+		options: {
+			nextConfigPath: resolve(__dirname, '../../../apps/web/next.config.mjs'),
+		},
+	},
+	docs: {
+		autodocs: 'tag',
+	},
+}
+export default config
